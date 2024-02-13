39c722b1d98b21c417f7bfa642216c2b2f64a7c0
joshjayboy <you@example.com>
2024-02-13 18:05:01 +0100

    great

---
+++ python track/599. Minimum Index Sum of Two Lists/list.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        list3 = []
+        list4 = []
+        for i in list1:
+            list3.append(str((i)))
+        if i in list2:
+            list4.append(str((i)))
+        
+
+
